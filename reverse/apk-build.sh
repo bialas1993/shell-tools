2f2cbbeea591f34166a643d2ff3cdc58abed1788
Dawid Biały <dawid.bialy@kajware.pl>
2017-01-27 12:36:24 +0100

    fix

--- reverse/apk-build.sh
+++ reverse/apk-build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-
 if [[ $# -eq 1 && -f $1 ]]; then
 
 	APP_DIR=$( echo $1 | cut -c 1-$((${#1} - 4)) )
 
+	OUT_APP="final.apk"
 	./apktool b $APP_DIR
 
-	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore BIB.keystore $1 backinblack
+	OUTPUT_APP=$APP_DIR-mod.apk
+
+	cp $APP_DIR/dist/$1 ./$APP_DIR-mod.apk
+
+
+
+	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore BIB.keystore $OUTPUT_APP backinblack
 
 	# Verify
 	jarsigner -verify -verbose -certs $1
 
-	rm final.apk
+	if [[ -f $$OUTPUT_APP ]]; then
+		rm $$OUTPUT_APP
+	fi
 
 	# Align
-	/opt/android-sdk/build-tools/25.0.0/zipalign -v 4 $1 final.apk
+	/opt/android-sdk/build-tools/25.0.0/zipalign -v 4 $OUTPUT_APP final.apk
 
 	# Install
 	adb install -r final.apk
