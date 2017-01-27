966c4b75e405a2707c6e1583b7aa3a88e5b0763f
Dawid Biały <bialydawid93@wp.pl>
2017-01-27 15:43:05 +0100

    add apktool

--- reverse/apk-build.sh
+++ reverse/apk-build.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+KEY=$SCRIPTPATH/BIB.keystore
+APKTOOL=$SCRIPTPATH/apktool
+
+OUT_APP="/tmp/final.apk"
+
 if [[ $# -eq 1 && -f $1 ]]; then
 
 	APP_DIR=$( echo $1 | cut -c 1-$((${#1} - 4)) )
-
-	OUT_APP="final.apk"
-	./apktool b $APP_DIR
+	
+	$APKTOOL b $APP_DIR
 
 	OUTPUT_APP=$APP_DIR-mod.apk
 
-	cp $APP_DIR/dist/$1 ./$APP_DIR-mod.apk
+	cp $APP_DIR/dist/$1 $OUTPUT_APP
 
-
-
-	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore BIB.keystore $OUTPUT_APP backinblack
+	echo "Pass: asdf1234"
+	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEY $OUTPUT_APP backinblack
 
 	# Verify
 	jarsigner -verify -verbose -certs $1
 
-	if [[ -f $$OUTPUT_APP ]]; then
-		rm $$OUTPUT_APP
+	if [[ -f $OUTPUT_APP ]]; then
+		rm $OUTPUT_APP
 	fi
 
 	# Align
-	/opt/android-sdk/build-tools/25.0.0/zipalign -v 4 $OUTPUT_APP final.apk
+	zipalign -v 4 $PWD/$OUTPUT_APP $OUT_APP
 
 	# Install
-	adb install -r final.apk
+	adb install -r $OUT_APP
 else
 	echo "Brak aplikacji do zbudowania"
 fi
